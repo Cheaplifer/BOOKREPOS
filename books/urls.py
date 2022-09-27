0bc26dc54392ddf75da1029d7e0f6ce5ae066eb7
samurai <larichev.po@students.sch2.ru>
2022-09-27 03:39:49 -0700

    Fixed, now works

--- books/urls.py
+++ books/urls.py
@@ -4,10 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('book/1/', views.book1, name='book1'),
-    path('book/2/', views.book1, name='book1'),
-    path('book/3/', views.book2, name='book2'),
-    path('book/4/', views.book3, name='book3'),
-    path('book/5/', views.book4, name='book4'),
-    path('book/6/', views.book5, name='book5'),
+    path('<int:book_id>/', views.get_book, name='view_book'),
 ]
--- books/views.py
+++ books/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Book
 from django.template import loader
+from django.shortcuts import get_object_or_404
 def index(request):
     latest_books_list = Book.objects.order_by('book_name')[:5]
     template = loader.get_template('index.html')
@@ -10,18 +11,6 @@ def index(request):
     }
     return HttpResponse(template.render(context, request))
 
-def book1(request):
-    return render(request, '1.html') #конечно же, это далется передачей параметра num в book и потом просто парсингом этого числа в строку, но мне лень
-    #теперь это TODO))))))))))))))))))))))))))))))))))))))))
-
-def book2(request):
-    return render(request, '2.html')
-
-def book3(request):
-    return render(request, '3.html')
-
-def book4(request):
-    return render(request, '4.html')
-
-def book5(request):
-    return render(request, '5.html')
+def get_book(request, book_id):
+    book = get_object_or_404(Book, pk = book_id)
+    return render(request, 'book.html', {'book': book})
