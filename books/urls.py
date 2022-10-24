33a0118a35cd04742e3b83ff849b05f7878900a7
Cheaplifer <larichev.po@students.sch2.ru>
2022-10-24 18:47:20 +0300

    hh

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:book_id>/', views.get_book, name='view_book'),
+    re_path(r'^delete/(?P<pk>[0-9]+)/$', views.delete_book, name='delete_book'),
+    re_path(r'^delete/author/(?P<pk>[0-9]+)/$', views.delete_author, name='delete_author') #я это скопипастил, надеюсь работает
 ]
--- books/views.py
+++ books/views.py
@@ -15,20 +15,20 @@ def index(request):
         }
         return HttpResponse(template.render(context, request))
     if request.method == 'POST':
-        aname = request.POST.get('aname', '')
-        surname = request.POST.get('surname', '')
-        if aname != None and surname != None:
+        current_request_name = request.POST.get('aname', '')
+        current_request_surname = request.POST.get('surname', '')
+        if current_request_name != None and current_request_surname != None:
             try:
-                au = Author.objects.get(name=aname, surname=surname)
+                au = Author.objects.get(name=current_request_name, surname=current_request_surname)
             except:
                 au = None
                 pass
             if au == None:
-                author = Author.objects.create(name=aname, surname=surname)
+                author = Author.objects.create(name=current_request_name, surname=current_request_surname)
         bname = request.POST.get('bname', '')
         if bname != None:
             text = request.POST.get('text', '')
-            author = Author.objects.get(name=aname, surname=surname)
+            author = Author.objects.get(name=current_request_name, current_request_surname=surname)
             book = Book.objects.create(name=bname, text=text, author=author)
             print(bname)
             print(book.name)
@@ -38,3 +38,25 @@ def index(request):
 def get_book(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
     return render(request, 'book.html', {'book': book})
+
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk = pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('/')
+    return render(request, 'index.html', {'book': book})
+
+
+def delete_author(request, pk):
+    authoreq = get_object_or_404(Author, pk=pk)  
+    if request.method == 'POST':         
+        authoreq.delete()
+        book_list = Book.objects.order_by('-name')
+        for i in book_list:
+            if i.author == authoreq
+                i.delete()
+        return redirect('/')
+
+    return render(request, 'index.html', {'author': authoreq})
+
+
